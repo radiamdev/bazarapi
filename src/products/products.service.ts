4b426220e5cc033f6293e505bbe035a5246cf4cb
radiamdev <166991055+radiamdev@users.noreply.github.com>
2025-06-02 20:50:58 +0300

    part 17 done

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -31,16 +31,55 @@ export class ProductsService {
         return await this.productRepository.save(product)
     }
 
-    findAll() {
-        return `This action returns all products`
+    async findAll(): Promise<ProductEntity[]> {
+        return await this.productRepository.find()
     }
 
-    findOne(id: number) {
-        return `This action returns a #${id} product`
+    async findOne(id: number) {
+        const product = await this.productRepository.findOne({
+            where: { id: id },
+            relations: {
+                addedBy: true,
+                category: true,
+            },
+            select: {
+                addedBy: {
+                    id: true,
+                    name: true,
+                    email: true,
+                },
+                category: {
+                    id: true,
+                    title: true,
+                },
+            },
+        })
+        if (!product) {
+            throw new NotFoundException('Product not found')
+        } else return product
     }
 
-    update(id: number, updateProductDto: UpdateProductDto) {
-        return `This action updates a #${id} product`
+    async update(
+        id: number,
+        updateProductDto: Partial<UpdateProductDto>,
+        currentUser: UserEntity
+    ): Promise<ProductEntity> {
+        const product = await this.findOne(id)
+
+        Object.assign(product, updateProductDto)
+        product.addedBy = currentUser
+
+        if (updateProductDto.categoryId) {
+            const category = await this.categoryService.findOne(
+                +updateProductDto.categoryId
+            )
+            if (!category) {
+                throw new NotFoundException('Category not found')
+            }
+            product.category = category
+        }
+
+        return await this.productRepository.save(product)
     }
 
     remove(id: number) {
