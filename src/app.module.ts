5847f180eaf93f7f346206b91b14bd8a6b464a01
radiamdev <166991055+radiamdev@users.noreply.github.com>
2025-06-11 21:39:47 +0300

    part 19 done

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule } from '@nestjs/config';
 import { CurrentUserMiddleware } from './utility/middlewares/current-user-middleware';
 import { CategoriesModule } from './categories/categories.module';
 import { ProductsModule } from './products/products.module';
+import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { ProductsModule } from './products/products.module';
     UsersModule,
     CategoriesModule,
     ProductsModule,
+    ReviewsModule,
   ],
   controllers: [],
   providers: [],
