343abf362acd39b5b6f7268b625c183c61f5a878
radiamdev <166991055+radiamdev@users.noreply.github.com>
2025-07-09 16:10:22 +0300

    feat: part 20 done

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -3,17 +3,20 @@ import {
     Get,
     Post,
     Body,
-    Patch,
     Param,
     Delete,
     UseGuards,
+    Put,
 } from '@nestjs/common'
 import { OrdersService } from './orders.service'
 import { CreateOrderDto } from './dto/create-order.dto'
-import { UpdateOrderDto } from './dto/update-order.dto'
 import { AuthenticationGuard } from 'src/utility/guards/authentication.guard'
+import { Roles } from 'src/utility/common/user-roles.enum'
+import { AuthorizeGuard } from 'src/utility/guards/authorization.guard'
 import { CurrentUser } from 'src/utility/decorators/current-user-decorator'
 import { UserEntity } from 'src/users/entities/user.entity'
+import { OrderEntity } from './entities/order.entity'
+import { UpdateOrderStatusDto } from './dto/update-order-status.dto'
 
 @Controller('orders')
 export class OrdersController {
@@ -21,23 +24,33 @@ export class OrdersController {
 
     @UseGuards(AuthenticationGuard)
     @Post()
-    async create(@Body() createOrderDto: CreateOrderDto, @CurrentUser() currentUser: UserEntity) {
+    async create(
+        @Body() createOrderDto: CreateOrderDto,
+        @CurrentUser() currentUser: UserEntity
+    ): Promise<OrderEntity | null> {
         return await this.ordersService.create(createOrderDto, currentUser)
     }
 
     @Get()
-    findAll() {
-        return this.ordersService.findAll()
+    async findAll(): Promise<OrderEntity[]> {
+        return await this.ordersService.findAll()
     }
 
     @Get(':id')
-    async findOne(@Param('id') id: string) {
+    async findOne(@Param('id') id: string): Promise<OrderEntity | null> {
         return await this.ordersService.findOne(+id)
     }
 
-    @Patch(':id')
-    update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
-        return this.ordersService.update(+id, updateOrderDto)
+    @UseGuards(AuthenticationGuard, AuthorizeGuard([Roles.ADMIN]))
+    @Put(':id')
+    async update(@Param('id') id: string, @Body() updateOrderStatusDto: UpdateOrderStatusDto, @CurrentUser() currentUser: UserEntity) {
+        return await this.ordersService.update(+id, updateOrderStatusDto, currentUser)
+    }
+
+    @UseGuards(AuthenticationGuard, AuthorizeGuard([Roles.ADMIN]))
+    @Put('cancel/:id')
+    async cancelled(@Param('id') id: string, @CurrentUser() currentUser: UserEntity) {
+        return await this.ordersService.cancelled(+id, currentUser)
     }
 
     @Delete(':id')
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,6 +1,9 @@
-import { Injectable } from '@nestjs/common'
+import {
+    BadRequestException,
+    Injectable,
+    NotFoundException,
+} from '@nestjs/common'
 import { CreateOrderDto } from './dto/create-order.dto'
-import { UpdateOrderDto } from './dto/update-order.dto'
 import { InjectRepository } from '@nestjs/typeorm'
 import { OrderEntity } from './entities/order.entity'
 import { Repository } from 'typeorm'
@@ -9,6 +12,8 @@ import { UserEntity } from 'src/users/entities/user.entity'
 import { ShippingEntity } from './entities/shipping.entity'
 import { ProductEntity } from 'src/products/entities/product.entity'
 import { ProductsService } from 'src/products/products.service'
+import { UpdateOrderStatusDto } from './dto/update-order-status.dto'
+import { OrderStatus } from './enums/order-status.enum'
 
 @Injectable()
 export class OrdersService {
@@ -20,7 +25,10 @@ export class OrdersService {
         private readonly productService: ProductsService
     ) {}
 
-    async create(createOrderDto: CreateOrderDto, currentUser: UserEntity) {
+    async create(
+        createOrderDto: CreateOrderDto,
+        currentUser: UserEntity
+    ): Promise<OrderEntity | null> {
         const shippingEntity = new ShippingEntity()
         Object.assign(shippingEntity, createOrderDto.shippingAddress)
 
@@ -62,11 +70,17 @@ export class OrdersService {
         return await this.findOne(orderTbl.id)
     }
 
-    findAll() {
-        return `This action returns all orders`
+    async findAll(): Promise<OrderEntity[]> {
+        return await this.orderRepository.find({
+            relations: {
+                shippingAddress: true,
+                user: true,
+                products: { product: true },
+            },
+        })
     }
 
-    async findOne(id: number) {
+    async findOne(id: number): Promise<OrderEntity | null> {
         return await this.orderRepository.findOne({
             where: { id },
             relations: {
@@ -77,11 +91,79 @@ export class OrdersService {
         })
     }
 
-    update(id: number, updateOrderDto: UpdateOrderDto) {
-        return `This action updates a #${id} order`
+    async update(
+        id: number,
+        updateOrderStatusDto: UpdateOrderStatusDto,
+        currentUser: UserEntity
+    ) {
+        let order = await this.findOne(id)
+        if (!order) throw new NotFoundException('Order not found')
+
+        if (
+            order.status === OrderStatus.DELIVERED ||
+            order.status === OrderStatus.CANCELLED
+        ) {
+            throw new BadRequestException(`Order already ${order.status}`)
+        }
+
+        if (
+            order.status === OrderStatus.PROCESSING &&
+            updateOrderStatusDto.status != OrderStatus.SHIPPED
+        ) {
+            throw new BadRequestException(`Delivery before shipped !!!`)
+        }
+
+        if (
+            updateOrderStatusDto.status === OrderStatus.SHIPPED &&
+            order.status === OrderStatus.SHIPPED
+        ) {
+            return order
+        }
+
+        if (updateOrderStatusDto.status === OrderStatus.SHIPPED) {
+            order.shippedAt = new Date()
+        }
+
+        if (updateOrderStatusDto.status === OrderStatus.DELIVERED) {
+            order.deliveredAt = new Date()
+        }
+
+        order.status = updateOrderStatusDto.status
+        order.updatedBy = currentUser
+        order = await this.orderRepository.save(order)
+
+        if (updateOrderStatusDto.status === OrderStatus.DELIVERED) {
+            await this.stockUpdate(order, OrderStatus.DELIVERED)
+        }
+
+        return order
+    }
+
+    async cancelled(id: number, currentUser: UserEntity) {
+        let order = await this.findOne(id)
+        if (!order) throw new NotFoundException('Order not found')
+
+        if (order.status === OrderStatus.CANCELLED) return order
+
+        order.status = OrderStatus.CANCELLED
+        order.updatedBy = currentUser
+        order = await this.orderRepository.save(order)
+        await this.stockUpdate(order, OrderStatus.CANCELLED)
+
+        return order
     }
 
     remove(id: number) {
         return `This action removes a #${id} order`
+    }
+
+    async stockUpdate(order: OrderEntity, status: string) {
+        for (const op of order.products) {
+            await this.productService.updateStock(
+                op.product.id,
+                op.product_quantity,
+                status
+            )
+        }
     }
 }
